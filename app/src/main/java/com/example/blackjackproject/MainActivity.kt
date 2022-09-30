732e027d6cf8c7fa61e55c088b1e9d6d3a4ed85b
mandeljam <113976430+mandeljam@users.noreply.github.com>
2022-10-01 00:28:12 +0200

    New UI, improved UX and restart upon finish

--- app/src/main/java/com/example/blackjackproject/MainActivity.kt
+++ app/src/main/java/com/example/blackjackproject/MainActivity.kt
@@ -1,24 +1,30 @@
 package com.example.blackjackproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.w3c.dom.Text
 import kotlin.math.absoluteValue
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var resultView : TextView
+    lateinit var playerResultView : TextView
+    lateinit var dealerResultView : TextView
     lateinit var cl : ConstraintLayout
     var deck = mutableListOf<Card>()
-//    val dealerCard1 = deck.random()
-//    val dealerCard2 = deck.random()
-//    val playerCard1 = deck.random()
-//    val playerCard2 = deck.random()
+    var dealerHand = 0
+    var playerHand = 0
+    var playerScore = 0
+    var dealerScore = 0
 
 
 
@@ -26,16 +32,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        resultView = findViewById(R.id.resultView)
-        val playButton = findViewById<Button>(R.id.playButton)
+        dealerResultView = findViewById(R.id.dealerResultView)
+        playerResultView = findViewById(R.id.playerResultView)
         val hitButton = findViewById<Button>(R.id.hitButton)
         val stayButton = findViewById<Button>(R.id.stayButton)
         val playerView1 = findViewById<ImageView>(R.id.playerView1)
-        val playerView2 = findViewById<ImageView>(R.id.playerView2)
-        val playerView3 = findViewById<ImageView>(R.id.playerView3)
         val dealerView1 = findViewById<ImageView>(R.id.dealerView1)
-        val dealerView2 = findViewById<ImageView>(R.id.dealerView2)
-        val dealerView3 = findViewById<ImageView>(R.id.dealerView3)
+        val dealerScoreView = findViewById<TextView>(R.id.dealerScoreText)
+        val playerScoreView = findViewById<TextView>(R.id.playerScoreText)
+        var score = intent.getIntExtra("dealerScore", 0)
 
 
         cl = findViewById(R.id.constraint_Layout)
@@ -45,40 +50,45 @@ class MainActivity : AppCompatActivity() {
         deck.shuffle()
         backgroundColor()
 
-        playButton.setOnClickListener {
-            val dealerCard1 = deck[0]
-            val dealerCard2 = deck[1]
-            val playerCard1 = deck[2]
-            val playerCard2 = deck[3]
-
-            dealerView1.setImageResource(dealerCard1.image)
-            dealerView2.setImageResource(dealerCard2.image)
-            playerView1.setImageResource(playerCard1.image)
-            playerView2.setImageResource(playerCard2.image)
-
-        }
-
         hitButton.setOnClickListener {
-            val playerCard3 = deck[4]
+            val playerCard = deck.random()
+            playerView1.setImageResource(playerCard.image)
+            playerHand += playerCard.value
+            playerResultView.text = "$playerHand"
+            if (playerHand > 21) {
+                playerResultView.text = "BUST"
+                startActivity(intent)
+            } else if (playerHand == 21) {
+                playerResultView.text = "BLACKJACK!"
 
-            playerView3.setImageResource(playerCard3.image)
+            }
 
         }
-        stayButton.setOnClickListener {
-            val dealerCard3 = deck[5]
+        stayButton.setOnClickListener{
+            GlobalScope.launch(Dispatchers.Main) {
+                while (dealerHand <= 16) {
+                    val dealerCard = deck.random()
+                    dealerView1.setImageResource(dealerCard.image)
+                    dealerHand += dealerCard.value
+                    dealerResultView.text = "$dealerHand"
+                    delay(500)
+                }
+                if (dealerHand > 21) {
+                    dealerResultView.text = "BUST"
 
-            dealerView3.setImageResource(dealerCard3.image)
+                } else if (dealerHand == 21) {
+                    dealerResultView.text = "$dealerHand BLACKJACK!"
+                } else if (dealerHand > playerHand) {
+                    dealerScore ++
+                    dealerScoreView.text = "Dealer Score: $dealerScore"
+                }
+            }
         }
-//        if (playerSumHit() > 21) {
-//            resultView.text = "BLACKJACK NOT"
-//        }
-//        when (21) {
-//            dealerSumStay() -> resultView.text = "BLACKJACK"
-//            dealerSumHit() -> resultView.text = "BLACKJACK"
-//            playerSumHit() -> resultView.text = "BLACKJACK"
-//            playerSumStay() -> resultView.text = "BLACKJACK"
-//            dealerSumHit() -> Log.d("!!!", "BLACKJACK")
-//        }
+//        if (dealerHand > playerHand) {
+//            playerResultView.text = "Dealer Won!"
+//        }else if (dealerHand < playerHand){
+//            dealerResultView.text = "Player Won!"
+//        }else "DRAW"
 
     }
     fun createDeck() {
@@ -114,20 +124,23 @@ class MainActivity : AppCompatActivity() {
     fun backgroundColor() {
         cl.setBackgroundResource(R.color.teal_200)
     }
-    fun dealerSumHit(): Int {
-        val dealerHand = deck[0].value + deck[1].value + deck[4].value
-        return dealerHand
+    fun draw(){
+
     }
-    fun dealerSumStay(): Int {
-        val dealerHandStay = deck[0].value + deck[1].value
-        return dealerHandStay
-    }
-    fun playerSumStay(): Int {
-        val playerHand = deck[2].value + deck[3].value
-        return playerHand
-    }
-    fun playerSumHit(): Int {
-        val playerHand = deck[2].value + deck[3].value + deck[5].value
-        return playerHand
-    }
+//    fun dealerSumHit(): Int {
+//        val dealerHand = deck[0].value + deck[1].value + deck[4].value
+//        return dealerHand
+//    }
+//    fun dealerSumStay(): Int {
+//        val dealerHandStay = deck[0].value + deck[1].value
+//        return dealerHandStay
+//    }
+//    fun playerSumStay(): Int {
+//        val playerHand = deck[2].value + deck[3].value
+//        return playerHand
+//    }
+//    fun playerSumHit(): Int {
+//        val playerHand = deck[2].value + deck[3].value + deck[5].value
+//        return playerHand
+//    }
 }
